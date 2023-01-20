579404a9a02cd1d8a7b4546f14522035161a0abd
Cristian Anderson Oliveira Bernardes <cristian.aob@gmail.com>
2023-01-20 11:56:05 -0300

    Update SaleRepository.php

--- app/Repositories/SaleRepository.php
+++ app/Repositories/SaleRepository.php
@@ -160,7 +160,7 @@ class SaleRepository
             ->when($salesman, function ($query, $salesman) {
                 $query->where('users.name', $salesman);
             })->when($startEndDate, function ($q, $startEndDate) {
-                $q->whereBetween('sales.created_at', $startEndDate);
+                $q->whereBetween('sales.date_hour_sale', $startEndDate);
             });
     }
 
