09331533492db5dc9c1dd8069efe22b69040319e
Cristian Anderson Oliveira Bernardes <cristian.aob@gmail.com>
2023-02-11 05:02:57 -0300

    Update AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
-use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
